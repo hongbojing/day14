136ad0d3425ed1cac52dcd618fcc7bc6fe747d4d
Hongbo Jing <jack.jing7@gmail.com>
2015-07-10 02:21:30 -0400

    death note cover

---
+++ builds/development/js/lib/app.js
@@ -0,0 +1,20 @@
+var paper=Raphael("death_note",450,400);
+
+var note_name=paper.text(110,90,"DEATH NOTE").attr({
+  "fill":"#FFF",
+  "font-size":50,
+  "text-anchor":"start",
+  "font-family":"deathNote"
+});
+var belief=paper.text(110,190,"first version").attr({
+  "fill":"#FFF",
+  "font-size":30,
+  "text-anchor":"start",
+  "font-family":"deathNote"
+});
+var name=paper.text(110,220,"--by i dont dare to write my name").attr({
+  "fill":"#FFF",
+  "font-size":20,
+  "text-anchor":"start",
+  "font-family":"deathNote"
+});
